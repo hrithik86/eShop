3f2e88e679055877bd2c0aba312ad8eb0b865ad9
hrithik86 <hrithikmalik86@gmail.com>
2019-02-12 22:34:45 +0530

    added file system

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,27 @@
 const   express=require("express"),
         router=express.Router();
+        fs=require("fs");
 
-const products=[];
+// const products=[];
 
 router.get("/add-product",(req,res)=>{
 res.render("add-product",{pageTitle:"Add product"});
 });
 
 router.post("/add-product",(req,res)=>{
-    console.log(req.body.title);
     product={
         title:req.body.title
     }
-products.push(product);
+// products.push(product);
+var data=(JSON.stringify(product));
+fs.appendFile("storage.txt",data,(err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("saved");
+})
 res.redirect("/");
 })
 
 exports.routes=router;
-exports.products=products;
+// exports.products=products;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const router=express.Router();
-const adminData=require("./admin");
+// const adminData=require("./admin");
+const fs=require("fs");
 
 router.get("/",(req,res)=>{
-    const products=adminData.products;
+var products =  fs.readFileSync("storage.txt",'utf8');
+console.log(products);    
+    // const products=adminData.products;
     res.render("shop",{products:products,pageTitle:"Shop"})
 });
 
