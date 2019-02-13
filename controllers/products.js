b8e4335b4e473236f0967b90e7f96b3d47deeb28
hrithik86 <hrithikmalik86@gmail.com>
2019-02-13 22:30:20 +0530

    added models

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,13 @@
-const products=[];
-
+const Product=require("../models/product");
 exports.getAddProduct = (req,res)=>{
     res.render("add-product",{pageTitle:"Add product"});
 }
 exports.postAddProduct=(req,res)=>{
-    product={
-        title:req.body.title
-    }
-products.push(product);
-res.redirect("/");
+    const product=new Product(req.body.title);
+    product.save();
+    res.redirect("/");
 }
 exports.getProducts=(req,res)=>{
+    const products=Product.fetchAll();
     res.render("shop",{products:products,pageTitle:"Shop"})
 }
---
+++ models/product.js
@@ -0,0 +1,15 @@
+const fs=require("fs");
+const path=require("path");
+const products=[];
+
+module.exports=class Product{
+    constructor(t){
+        this.title=t;
+    }
+    save(){
+        products.push(this);
+    }
+    static fetchAll(){
+        return products;
+    }
+}
