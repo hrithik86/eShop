138c3abbfb33c3ccce47a12c709a822eb27f4882
hrithik86 <hrithikmalik86@gmail.com>
2019-02-15 18:12:31 +0530

    added shop structure

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
 const Product=require("../models/product");
 exports.getAddProduct = (req,res)=>{
-    res.render("add-product",{pageTitle:"Add product"});
+    res.render("admin/add-product",{pageTitle:"Add product"});
 }
 exports.postAddProduct=(req,res)=>{
     const product=new Product(req.body.title);
@@ -9,7 +9,7 @@ exports.postAddProduct=(req,res)=>{
 }
 exports.getProducts=(req,res)=>{
     Product.fetchAll(products=>{
-        res.render("shop",{products:products,pageTitle:"Shop"})
+        res.render("shop/product-list",{products:products,pageTitle:"Shop"})
     });
    
 }
